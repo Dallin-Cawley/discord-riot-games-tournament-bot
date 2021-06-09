b32f3b1b56768bac9da48ffd7b9750ad09dca35b
Dallin Cawley <dallincawley@gmail.com>
2021-06-08 23:34:08 -0600

    Added retrieval of riot.txt

--- src/main/java/RiotGamesDiscordBot/TournamentBotDriver.java
+++ src/main/java/RiotGamesDiscordBot/TournamentBotDriver.java
@@ -5,9 +5,18 @@ import RiotGamesDiscordBot.Tournament.TournamentManager;
 import com.google.gson.Gson;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.TextChannel;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -36,5 +45,21 @@ public class TournamentBotDriver {
 
 
         return "Done";
+    }
+
+    @RequestMapping(value = "/riotVerify")
+    public ResponseEntity<Object> downloadFile() throws IOException  {
+        String filename = "/riot.txt";
+        File file = new File(filename);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        return ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(
+        MediaType.parseMediaType("application/txt")).body(resource);
     }
 }
